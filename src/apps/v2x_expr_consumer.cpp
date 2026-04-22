db1f99efc525ef23be5a99f5c8e844b7de2c2eff
ESeokhwan <huan99@hanmail.net>
2026-04-22 21:46:58 +0900

    feat: update consume_run to use current timestamp instead of message timestamp

--- src/apps/v2x_expr_consumer.cpp
+++ src/apps/v2x_expr_consumer.cpp
@@ -284,10 +284,9 @@ void consume_run(struct ConsumerThreadArg *arg) {
 
     while (!arg->end_flag->load(memory_order_acquire)) {
         RdKafka::Message *msg = consumer::consume_message(consumer.get(), arg->poll_timeout);
+        int64_t responded_at = util::get_current_timestamp();
         if (msg == nullptr) continue;
         std::string plain_msg = std::string(static_cast<const char*>(msg->payload()));
-        RdKafka::MessageTimestamp ts = msg->timestamp();
-        int64_t responded_at = ts.timestamp;
         delete msg;
 
         if (arg->log_disabled) continue;
